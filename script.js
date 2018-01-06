8809eae8bc2d74d24d161381c09508da61d66732
irbekrm <irbekrm@gmail.com>
2018-01-06 07:50:15 +0100

    add div to svg

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //TODO: provide a default quote if promise is rejected
-const request = url => new Promise(res => {
+const request = url => new Promise((res,rej) => {
   const xhr = new XMLHttpRequest();
   xhr.open('get', url, true);
   xhr.onreadystatechange = _ =>
@@ -11,7 +11,8 @@ const request = url => new Promise(res => {
 async function printStuff() {
   console.log("WORKING");
   const quote = await request("http://quotes.stormconsultancy.co.uk/random.json");
-  console.log("Random quote is ", quote);
+  
+  
 }
 
 const makeBackground = _ => 
@@ -46,8 +47,16 @@ const randomRotation = _ => {let y=getRandInt(0,360); d3.selectAll("text")
                               .attr("transform", _ => `rotate(${getRandInt(360)})`); }
 const animate = _ => setInterval(randomRotation, 1000);
 
-const doStuff = _ => { printStuff(); makeCharsArray(); makeBackground(); }
+const addBox = _ => d3.select("svg")
+                      .append("foreignObject")
+                      .attr("x", "100")
+                      .attr("y", "100")
+                      .attr("width", "200")
+                      .append("xhtml:div")
+                      .append("div")
+                      .attr("class", "quoteBox")
+
+const doStuff = _ => { makeCharsArray(); makeBackground(); printStuff(); addBox(); }
 
 
 window.onload = doStuff; 
-animate();
